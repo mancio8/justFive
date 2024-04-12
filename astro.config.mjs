0e2d6ab1167d8990f1eab8667e055776a0967774
mancio8 <64028211+mancio8@users.noreply.github.com>
2024-04-12 22:12:08 +0200

    peornf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
